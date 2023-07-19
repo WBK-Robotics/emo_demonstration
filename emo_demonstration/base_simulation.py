3a2732799a4ec2ef1ddf7f66de70ef230231738d
Jan Baumgärnter <22688144+liquidcronos@users.noreply.github.com>
2023-07-19 10:06:43 +0200

    adjusted base simulation to new gripper config

--- emo_demonstration/base_simulation.py
+++ emo_demonstration/base_simulation.py
@@ -5,11 +5,10 @@ import pybullet_industrial as pi
 
 
 
-def setup_base_sim(mode = "gui",returnWhiteboardID = False):
+def setup_base_sim(mode = "gui"):
     file_directory = os.path.dirname(os.path.abspath(__file__))
     sdmbot_urdf_file = os.path.join(file_directory, 'urdf', 'sdmbot.urdf')
-    endeffector_file = os.path.join(
-        file_directory, 'urdf', 'welding_torch.urdf')
+    endeffector_urdf_file = os.path.join(file_directory, 'urdf', 'endeffector.urdf')
 
     if mode == "gui":
         physics_client = p.connect(p.GUI, options='--background_color_red=1 ' +
@@ -26,28 +25,15 @@ def setup_base_sim(mode = "gui",returnWhiteboardID = False):
     p.resetSimulation()
     p.setPhysicsEngineParameter(numSolverIterations=2000)
     p.setVRCameraState(rootPosition=(0,0,-0.50))
-    # spawn a a rectangular multi body
-    rectangle_id = p.createVisualShape(
-        p.GEOM_BOX, halfExtents=[0.15, 0.1, 0.01])
-    collision_id = p.createCollisionShape(
-        p.GEOM_BOX, halfExtents=[0.15, 0.1, 0.01])
-    whiteboardID = p.createMultiBody(
-        baseMass=0, baseVisualShapeIndex=rectangle_id,
-        baseCollisionShapeIndex=collision_id,
-        basePosition=[0, 0.15,  0.55])
+
 
     start_orientation = p.getQuaternionFromEuler([0, 0, 0])
 
-    extruder_properties = {'maximum distance': 0.1,
-                           'opening angle': 0,
-                           'material': CustomMaterial,
-                           'material properties': {'particle size': 0.005, 'color': [0.0, 0.0, 1, 1]},
-                           'number of rays': 1}
+  
 
     robot = pi.RobotBase(sdmbot_urdf_file, [0, 0, 0], start_orientation)
-    endeffector = pi.Extruder(
-        endeffector_file, [0, 0, 0], start_orientation, extruder_properties)
-    endeffector.couple(robot)
+    endeffector = pi.Gripper(endeffector_urdf_file, [0, 0, 0], start_orientation)
+    endeffector.couple(robot,endeffector_name='tool0')
 
     joint_state = {'shoulder_lift_joint': -1.3061111730388184,
                    'elbow_joint': -0.43253040313720703,
@@ -58,17 +44,24 @@ def setup_base_sim(mode = "gui",returnWhiteboardID = False):
 
     robot.set_joint_position(joint_state)
 
-    if(returnWhiteboardID):
-        return robot, endeffector, whiteboardID
+
     return robot, endeffector
 
 if __name__ == "__main__":
     import numpy as np
 
     robot, endeffector = setup_base_sim()
+    for _ in range(100):
+        p.stepSimulation()
 
     position = np.array([0, 0, 0.63])
     while True:
-        control_loop(endeffector, position)
         p.stepSimulation()
-        time.sleep(0.01)
+        endeffector.actuate(1)
+        time.sleep(1)
+        for _ in range(50):
+            p.stepSimulation()
+        endeffector.actuate(0)
+        time.sleep(1)
+        for _ in range(50):
+            p.stepSimulation()
